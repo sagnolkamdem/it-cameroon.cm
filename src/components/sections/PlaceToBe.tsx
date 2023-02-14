8208e6b97e4ec61d5a79c6be04e8dfab912b3ff9
sagnolkamdem <sagnolkamdem721@gmail.com>
2023-02-14 08:31:50 +0100

    :bug: deploy error
    deploy error

--- src/components/sections/PlaceToBe.tsx
+++ src/components/sections/PlaceToBe.tsx
@@ -5,13 +5,13 @@ import small from "../../public/logo/small.png";
 
 const PlaceToBe = () => {
     return ( 
-        <div className="flex flex-col gap-8 sm:grid lg:grid-cols-3 sm:grid-cols-2 items-center mt-14 overflow-hidden mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-col gap-8 sm:grid lg:grid-cols-3 sm:grid-cols-2 items-center mt-14 overflow-hidden mx-auto max-w-7xl pt-12 px-4 sm:px-6 lg:px-8">
 
             <div className="lg:col-span-2 max-w-2xl">
                 
                 <div className="mb-16">
                     <h2 className="font-display text-4xl font-bold tracking-tight text-slate-900 dark:text-white mb-5">A platform built by and for people like you</h2>
-                    <p className="text-xl font-semibold text-slate-500 dark:text-slate-300">Whether you want to participate in the country's technological transition by being a passive or active player, TECH CAMER is for you. Join us to discover the technological transition and enrich it.</p>
+                    <p className="text-xl font-semibold text-slate-500 dark:text-slate-300">Whether you want to participate in the country&apos;s technological transition by being a passive or active player, TECH CAMER is for you. Join us to discover the technological transition and enrich it.</p>
                 </div>
 
                 <div className="grid grid-cols-3 mt-10 gap-10">
@@ -56,7 +56,7 @@ const PlaceToBe = () => {
             </div>
 
             <div className="relative">
-                <Image height={500} width={400} className="object-cover border border-primary-600" src="https://images.unsplash.com/photo-1540304801084-0944beb20de7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cGVyc29ubmUlMjBub2lyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" alt="user" />
+                <Image height={500} width={400} className="object-cover" src="https://images.unsplash.com/photo-1540304801084-0944beb20de7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cGVyc29ubmUlMjBub2lyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" alt="user" />
             </div>
 
         </div>
